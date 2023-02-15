f4f80d8e516a8e6c0e2a6c4d7972d09d7e1d9db9
MuntasirAbid <muntasira7@hotmail.com>
2023-02-16 00:05:16 +0100

    inheritance

--- dist/oop.js
+++ dist/oop.js
@@ -8,5 +8,16 @@ class Player {
         console.log(`playerName: ${this.playerName}, age: ${this.age}`);
     }
 }
+class Student extends Player {
+    constructor(playerName, age, studentId) {
+        super(playerName, age);
+        this.studentId = studentId;
+    }
+    display() {
+        console.log(`studentName: ${this.playerName}, age: ${this.age} id: ${this.studentId}`);
+    }
+}
+let student1 = new Student('abid', 24, 39222333);
+student1.display();
 let player1 = new Player("jonn", 26);
 player1.display();
--- src/oop.ts
+++ src/oop.ts
@@ -7,11 +7,29 @@ class Player {
         this.age = age
     }
 
-    display() {
+    display(): void {
         console.log(`playerName: ${this.playerName}, age: ${this.age}`);
     }
 }
 
+class Student extends Player {
+    studentId: number;
+
+    constructor(playerName: string, age: number, studentId: number) {
+        super(playerName, age);
+        this.studentId = studentId
+    }
+
+    display(): void {
+        console.log(`studentName: ${this.playerName}, age: ${this.age} id: ${this.studentId}`);
+    }
+}
+
+let student1 = new Student('abid', 24, 39222333)
+student1.display()
+
 let player1 = new Player("jonn", 26)
 player1.display();
+
+
 
